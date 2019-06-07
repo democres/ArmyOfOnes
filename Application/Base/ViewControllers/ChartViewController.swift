80e184aa8536b563d05323f727c273f32a4f9e7a
Democres <democres@hotmail.es>
2019-06-07 06:37:28 -0500

    Limit Currencies

--- Application/Base/ViewControllers/ChartViewController.swift
+++ Application/Base/ViewControllers/ChartViewController.swift
@@ -56,12 +56,20 @@ class ChartViewController: UIViewController {
         let colors = [#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1), #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1), #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1), #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1), #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1), #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)]
         var result: [DataEntry] = []
         
-        guard let rates = self.currency?.rates else { return result}
+        let ratesArray = self.currency?.rates?.filter({ (key, _) -> Bool in
+            return key == "GBP" || key == "EUR" || key == "JPY" || key == "BRL"
+        })
+        
+        
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 2
+        
+        guard let rates = ratesArray else { return result}
         var i = 0
         for rate in rates {
-            let value = Float(rate.value as? NSNumber ?? 1)
-            let height: Float = Float(value) / 100.0
-            result.append(DataEntry(color: colors[i % colors.count], height: height, textValue:"\(value)", title: rate.key))
+            let value = Float(truncating: rate.value as? NSNumber ?? 1)
+            let height = value / ( value > 100 ? 1000 : 100)
+            result.append(DataEntry(color: colors[i % colors.count], height: height, textValue:formatter.string(from: NSNumber(value: value)) ?? "", title: rate.key))
             i += 1
         }
         
